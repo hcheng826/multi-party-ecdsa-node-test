203712932fbb01b34f4f2c86dee77ab11d9a0314
hcheng826 <lilioo826@gmail.com>
2022-12-11 22:28:46 +0800

    get address from pubkey

--- getAddressFromPubKey.cjs
+++ getAddressFromPubKey.cjs
@@ -1,14 +1,7 @@
-// Reference: https://programmer.ink/think/specific-process-of-ethereum-generating-address-from-public-key.html
-const keccak256 = require('keccak256')
 const { y_sum_s } = require("./const.cjs");
-const elliptic = require("elliptic");
-const ec = new elliptic.ec("secp256k1");
+const ethers = require("ethers");
 
-const pubKey = Buffer.from(y_sum_s);
-
-// remove the prefix 04: https://github.com/indutny/elliptic/issues/86
-const fullPubKey64byte = ec.keyFromPublic(pubKey).getPublic('hex').slice(2);
-
-const address = keccak256(fullPubKey64byte).toString('hex').slice(24);
-console.log(address.length);
-console.log("address: ", address);
+console.log(
+  "address: ",
+  ethers.utils.computeAddress("0x" + Buffer.from(y_sum_s).toString("hex"))
+);
